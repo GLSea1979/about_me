8ece88935eab5d2381734772d3020030f19a19c6
Gary Lundgren <builtbygl@gmail.com>
2016-11-29 15:09:51 -0800

    added content to html and modified questions

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 'use strict';
 
 // Design a guessing game about you that involves FIVE yes/no questions. Be creative and fun and whimsical. Or not. It's up to you! It's your project!
-// The questions in the guessing game must require a mix of yes/no answers, and user input must accept either y/n or yes/no responses, with either .toUpperCase() or .toLowerCase() used to validate the user input and accommodate users entering all-caps Y/N or YES/NO answers, too.
+// The questions in the guessing game must require a mix of yes/no answer1s, and user input must accept either y/n or yes/no responses, with either .toUpperCase() or .toLowerCase() used to validate the user input and accommodate users entering all-caps Y/N or YES/NO answers, too.
 
 
-var answer = prompt('Do Gary have any cats?').toLowerCase();
-if(answer != 0) {
-  if (answer === 'yes' || answer === 'y') {
+var answer1 = prompt('Do Gary have any cats?').toLowerCase();
+if( != 0) {
+  if (answer1 === 'yes' || answer1 === 'y') {
     alert('Oops, wrong Gary used to have a cat. But no more.');
   } else
-  if(answer === 'no' || answer === 'n') {
+  if(answer1 === 'no' || answer1 === 'n') {
     alert('Yep you are correct.');
   } else {
     alert('That was not a valid answer but we will move on');
